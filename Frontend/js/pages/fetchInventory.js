d90d1b6646ab8cdc6143639b54a4070810f7ef52
Carlos Salcán <cdsalcan131@gmail.com>
2024-10-27 22:26:19 -0500

    Mejoras en la tabla y acordeón
    - Se corrigió el padding y margen de las celdas del acordeón para asegurar que se visualicen correctamente sin espacios innecesarios.
    - Se revisaron y ajustaron los estilos de la tabla para una mejor presentación.

--- Frontend/js/pages/fetchInventory.js
+++ Frontend/js/pages/fetchInventory.js
@@ -6,8 +6,8 @@ function populateTable(equipos) {
     equipos.forEach((equipo, index) => {
         // Crear una fila para el equipo con los detalles en una sola fila
         const equipoRow = document.createElement('tr');
-        equipoRow.innerHTML = `
-            <tr>
+        equipoRow.innerHTML =
+            `<tr>
                 <th>Cod.</th>
                 <th>Fecha Reg.</th>
                 <th>Cod. Almacen</th>
@@ -21,8 +21,8 @@ function populateTable(equipos) {
         tableBody.appendChild(equipoRow);
 
         const detalleRow = document.createElement('tr');
-        detalleRow.innerHTML = `
-            <td>${equipo.cod_equipo}</td>
+        detalleRow.innerHTML =
+            `<td>${equipo.cod_equipo}</td>
             <td>${new Date(equipo.fec_reg).toLocaleDateString()}</td>
             <td>${equipo.cod_almacen}</td>
             <td>${equipo.tip_equipo}</td>
@@ -32,129 +32,163 @@ function populateTable(equipos) {
             <td>${equipo.nom_usua}</td>
             <td>
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick='openEditModal(${JSON.stringify(equipo)})'> E </button>
-            </td>
-        `;
+            </td>`;
         tableBody.appendChild(detalleRow);
 
-        // Cargar opciones para los select (pisos y servicios)
-    loadSelectOptions('pisos', 'modal-piso-ubic');
-    loadSelectOptions('servicios', 'modal-serv-depar');
+        // Crear acordeón para los componentes
+        const accordionRow = document.createElement('tr');
+        accordionRow.innerHTML =
+            `<td colspan="9" style="padding: 0; margin: 0;">
+                <div class="accordion" id="accordion-${index}">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCPU-${index}" aria-expanded="false" aria-controls="collapseCPU-${index}">
+                                CPU
+                            </button>
+                        </h2>
+                        <div id="collapseCPU-${index}" class="accordion-collapse collapse" data-bs-parent="#accordion-${index}">
+                            <div class="accordion-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Cod_Equipo</th>
+                                            <th>Cod_Tics</th>
+                                            <th>Marca</th>
+                                            <th>Procesador</th>
+                                            <th>Memoria</th>
+                                            <th>Almacenamiento</th>
+                                            <th>Editar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            ${equipo.cod_mouse ? `
+                                                <td>${equipo.cod_equipo}</td>
+                                                <td>${equipo.cod_tics_cpu}</td>
+                                                <td>${equipo.mar_cpu}</td>
+                                                <td>${equipo.procesador}</td>
+                                                <td>${equipo.memoria}</td>
+                                                <td>${equipo.tam_hdd}</td>
+                                                <td>Edicion</td>
+                                            ` : `<td colspan="6">No disponible</td>`}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
-        // Añadir detalles del monitor
-        const monitorRow = document.createElement('tr');
-        monitorRow.innerHTML = `
-            <td colspan="9"  style="background-color: #e0e0e0"><strong>Monitor</strong></td>
-        `;
-        tableBody.appendChild(monitorRow);
-
-        const monitorHeaderRow = document.createElement('tr');
-        monitorHeaderRow.innerHTML = `
-            <td>Cod_Equipo</td>
-            <td colspan="2">Cod_Tics</td>
-            <td>Marca</td>
-            <td colspan="2">Modelo</td>
-            <td>Tamaño</td>
-            <td colspan="2">Condición</td>
-        `;
-        tableBody.appendChild(monitorHeaderRow);
-
-        const monitorDetailsRow = document.createElement('tr');
-        if (equipo.cod_monitor) {
-            monitorDetailsRow.innerHTML = `
-                <td>${equipo.cod_equipo}</td>
-                <td colspan="2">${equipo.cod_tics_monitor}</td>
-                <td>${equipo.mar_monitor}</td>
-                <td colspan="2">${equipo.mod_monitor}</td>
-                <td>${equipo.tam_monitor}</td>
-                <td>${equipo.con_monitor}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> E </button>
-                </td>
-            `;
-        } else {
-            monitorDetailsRow.innerHTML = `
-                <td colspan="9">No disponible</td>
-            `;
-        }
-        tableBody.appendChild(monitorDetailsRow);
-
-        // Añadir detalles del teclado
-        const tecladoRow = document.createElement('tr');
-        tecladoRow.innerHTML = `
-            <td colspan="9" style="background-color: #e0e0e0"><strong>Teclado</strong></td>
-        `;
-        tableBody.appendChild(tecladoRow);
-
-        const tecladoHeaderRow = document.createElement('tr');
-        tecladoHeaderRow.innerHTML = `
-            <td>Cod_Equipo</td>
-            <td colspan="2">Cod_Tics</td>
-            <td>Marca</td>
-            <td colspan="2">Modelo</td>
-            <td>Puerto</td>
-            <td colspan="2">Condición</td>
-        `;
-        tableBody.appendChild(tecladoHeaderRow);
-
-        const tecladoDetailsRow = document.createElement('tr');
-        if (equipo.cod_teclado) {
-            tecladoDetailsRow.innerHTML = `
-                <td>${equipo.cod_equipo}</td>
-                <td colspan="2">${equipo.cod_tics_teclado}</td>
-                <td>${equipo.mar_teclado}</td>
-                <td colspan="2">${equipo.mod_teclado}</td>
-                <td>${equipo.pue_teclado}</td>
-                <td>${equipo.con_teclado}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> E </button>
-                </td>
-            `;
-        } else {
-            tecladoDetailsRow.innerHTML = `
-                <td colspan="9">No disponible</td>
-            `;
-        }
-        tableBody.appendChild(tecladoDetailsRow);
-
-        // Añadir detalles del mouse
-        const mouseRow = document.createElement('tr');
-        mouseRow.innerHTML = `
-            <td colspan="9" style="background-color: #e0e0e0"><strong>Mouse</strong></td>
-        `;
-        tableBody.appendChild(mouseRow);
-
-        const mouseHeaderRow = document.createElement('tr');
-        mouseHeaderRow.innerHTML = `
-            <td>Cod_Equipo</td>
-            <td colspan="2">Cod_Tics</td>
-            <td>Marca</td>
-            <td colspan="2">Modelo</td>
-            <td>Puerto</td>
-            <td colspan="2">Condición</td>
-        `;
-        tableBody.appendChild(mouseHeaderRow);
-
-        const mouseDetailsRow = document.createElement('tr');
-        if (equipo.cod_mouse) {
-            mouseDetailsRow.innerHTML = `
-                <td>${equipo.cod_equipo}</td>
-                <td colspan="2">${equipo.cod_tics_mouse}</td>
-                <td>${equipo.mar_mouse}</td>
-                <td colspan="2">${equipo.mod_mouse}</td>
-                <td>${equipo.pue_mouse}</td>
-                <td>${equipo.con_mouse}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> E </button>
-                </td>
-            `;
-        } else {
-            mouseDetailsRow.innerHTML = `
-                <td colspan="4">No disponible</td>
-            `;
-        }
-        tableBody.appendChild(mouseDetailsRow);
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMonitor-${index}" aria-expanded="true" aria-controls="collapseMonitor-${index}">
+                                Monitor
+                            </button>
+                        </h2>
+                        <div id="collapseMonitor-${index}" class="accordion-collapse collapse" data-bs-parent="#accordion-${index}">
+                            <div class="accordion-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Cod_Equipo</th>
+                                            <th>Cod_Tics</th>
+                                            <th>Marca</th>
+                                            <th>Modelo</th>
+                                            <th>Tamaño</th>
+                                            <th>Condición</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            ${equipo.cod_monitor ? `
+                                                <td>${equipo.cod_equipo}</td>
+                                                <td>${equipo.cod_tics_monitor}</td>
+                                                <td>${equipo.mar_monitor}</td>
+                                                <td>${equipo.mod_monitor}</td>
+                                                <td>${equipo.tam_monitor}</td>
+                                                <td>${equipo.con_monitor}</td>
+                                            ` : `<td colspan="6">No disponible</td>`}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTeclado-${index}" aria-expanded="false" aria-controls="collapseTeclado-${index}">
+                                Teclado
+                            </button>
+                        </h2>
+                        <div id="collapseTeclado-${index}" class="accordion-collapse collapse" data-bs-parent="#accordion-${index}">
+                            <div class="accordion-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Cod_Equipo</th>
+                                            <th>Cod_Tics</th>
+                                            <th>Marca</th>
+                                            <th>Modelo</th>
+                                            <th>Puerto</th>
+                                            <th>Condición</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            ${equipo.cod_teclado ? `
+                                                <td>${equipo.cod_equipo}</td>
+                                                <td>${equipo.cod_tics_teclado}</td>
+                                                <td>${equipo.mar_teclado}</td>
+                                                <td>${equipo.mod_teclado}</td>
+                                                <td>${equipo.pue_teclado}</td>
+                                                <td>${equipo.con_teclado}</td>
+                                            ` : `<td colspan="6">No disponible</td>`}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMouse-${index}" aria-expanded="false" aria-controls="collapseMouse-${index}">
+                                Mouse
+                            </button>
+                        </h2>
+                        <div id="collapseMouse-${index}" class="accordion-collapse collapse" data-bs-parent="#accordion-${index}">
+                            <div class="accordion-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Cod_Equipo</th>
+                                            <th>Cod_Tics</th>
+                                            <th>Marca</th>
+                                            <th>Modelo</th>
+                                            <th>Puerto</th>
+                                            <th>Condición</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            ${equipo.cod_mouse ? `
+                                                <td>${equipo.cod_equipo}</td>
+                                                <td>${equipo.cod_tics_mouse}</td>
+                                                <td>${equipo.mar_mouse}</td>
+                                                <td>${equipo.mod_mouse}</td>
+                                                <td>${equipo.pue_mouse}</td>
+                                                <td>${equipo.con_mouse}</td>
+                                            ` : `<td colspan="6">No disponible</td>`}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </td>`;
+        tableBody.appendChild(accordionRow);
     });
 }
+
 
 // fetchInventory.js
 async function fetchEquiposEscritorio() {
