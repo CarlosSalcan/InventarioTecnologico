927dc39748ddfdca1c5c3632fab8ef4e80d0c289
Carlos Salcán <cdsalcan131@gmail.com>
2024-11-05 11:21:00 -0500

    Corrección del comportamiento del modal al cerrarlo
    - Se agregó un event listener para el evento 'hidden.bs.modal' en el modal de edición.
    - Al cerrar el modal, se eliminan la clase 'modal-open' del cuerpo y el backdrop oscuro, permitiendo el uso normal del sitio web.
    - Se asegura que el modal se comporte correctamente al interactuar con los botones de cierre.

--- Frontend/js/components/modal.js
+++ Frontend/js/components/modal.js
@@ -1,8 +1,18 @@
-fetch('./components/modal.html') // Ruta component
-.then(response => response.text())
-.then(data => {
-    document.getElementById('modal-container').innerHTML = data;
-})
-.catch(error => console.error('Error al cargar el modal:', error));
+// Cargar el contenido del modal desde el archivo modal.html
+fetch('./components/modal.html') // Ruta del componente
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('modal-container').innerHTML = data;
 
+        // Añadir evento al botón de cerrar el modal
+        const modal = document.getElementById('exampleModal');
+        modal.addEventListener('hidden.bs.modal', function () {
+            document.body.classList.remove('modal-open');
+            const backdrop = document.querySelector('.modal-backdrop');
+            if (backdrop) {
+                backdrop.remove();
+            }
+        });
 
+    })
+    .catch(error => console.error('Error al cargar el modal:', error));
