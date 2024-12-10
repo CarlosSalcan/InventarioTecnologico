827211e664f54b86d8284d3557a4ec7e42d34ecf
Carlos Salcán <cdsalcan131@gmail.com>
2024-12-09 21:59:19 -0500

    Corrección de errores en el controller y adición de estilos CSS
    - Se corrigieron errores en la conexión a la base de datos y en el controller para asegurar la correcta recuperación de datos.
    - Se añadieron estilos CSS para una mejor organización hasta terminar pagina.
    
    TODO: editar, eliminar y agregar parametro

--- Frontend/js/pages/parameters.js
+++ Frontend/js/pages/parameters.js
@@ -59,6 +59,9 @@ function cargarTablaSeleccionada(tabla) {
         case 'param_condicion':
             mostrarParametros('param_condicion', 'cod_condicion', 'nom_condicion', 'searchContainer');
             break;
+        case 'param_dis_opt':
+            mostrarParametros('param_dis_opt', 'cod_dis_opt', 'nom_dis_opt', 'searchContainer');
+            break;
         case 'param_estado':
             mostrarParametros('param_estado', 'cod_estado', 'nom_estado', 'searchContainer');
             break;
@@ -68,17 +71,44 @@ function cargarTablaSeleccionada(tabla) {
         case 'param_memoria':
             mostrarParametros('param_memoria', 'cod_memoria', 'nom_memoria', 'searchContainer');
             break;
+        case 'param_num_hdd':
+            mostrarParametros('param_num_hdd', 'cod_n_hdd', 'nom_n_hdd', 'searchContainer');
+            break;
         case 'param_office':
             mostrarParametros('param_office', 'cod_office', 'nom_office', 'searchContainer');
             break;
         case 'param_piso':
             mostrarParametros('param_piso', 'cod_piso', 'nom_piso', 'searchContainer');
             break;
+        case 'param_procesador':
+            mostrarParametros('param_procesador', 'cod_proce', 'nom_proce', 'searchContainer');
+            break;
+        case 'param_puertos':
+            mostrarParametros('param_puertos', 'cod_puerto', 'nom_puerto', 'searchContainer');
+            break;
+        case 'param_servicio':
+            mostrarParametros('param_servicio', 'cod_servicio', 'nom_servicio', 'searchContainer');
+            break;
         case 'param_sis_ope':
             mostrarParametros('param_sis_ope', 'cod_sis_ope', 'nom_sis_ope', 'searchContainer');
             break;
         case 'param_tamano_hdd':
             mostrarParametros('param_tamano_hdd', 'cod_tam_hdd', 'nom_tam_hdd', 'searchContainer');
+            break;
+        case 'param_tamano_monitor':
+            mostrarParametros('param_tamano_monitor', 'cod_tam_mon', 'nom_tam_mon', 'searchContainer');
+            break;
+        case 'param_tipo_equipo':
+            mostrarParametros('param_tipo_equipo', 'cod_te', 'nom_te', 'searchContainer');
+            break;
+        case 'param_tipo_impresora':
+            mostrarParametros('param_tipo_impresora', 'cod_ti', 'nom_ti', 'searchContainer');
+            break;
+        case 'param_tipo_monitor':
+            mostrarParametros('param_tipo_monitor', 'cod_tm', 'nom_tm', 'searchContainer');
+            break;
+        case 'param_tipo_mt':
+            mostrarParametros('param_tipo_mt', 'cod_tmt', 'nom_tmt', 'searchContainer');
             break;
         default:
             document.getElementById('searchContainer').innerHTML = '<p>Seleccione una tabla para mostrar.</p>';
